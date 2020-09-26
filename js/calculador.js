1b1d1271a61b10037a80218e361171da12b5d0d4
Facundo Alavar <facundo.alavar@webexport.com.ar>
2020-09-25 23:04:34 -0300

    fix calculador

--- js/calculador.js
+++ js/calculador.js
@@ -4,12 +4,20 @@ $(document).ready(function(){
     });
 });
 
-let productos = {
-    'asadosurtido': [1,0.5, 0.5],
-    'chorizosymorcillas': [2,0.5, 0.5],
-    'colitacuadril': [2,0.5, 0.5],
-    'achuras': [12,0.5, 0.5],
-};
+let productos = [
+    {
+        'texto': 'Asado Surtido',
+        'cantidades': [3, 2, 1]
+    },
+    {
+        'texto': 'Chorizos y Morcillas',
+        'cantidades': [1,0.5, 0.5]
+    },
+    {
+        'texto': 'Colita de Cuadril',
+        'cantidades': [1,0.5, 0.5]
+    },
+];
 
 function calcular(){
     items = $('#items');
@@ -27,9 +35,18 @@ function calcular(){
     if (!menores) {
         menores = 0;
     }
-    
-    Object.keys(productos).forEach(element => {
 
+    items.html('');
+
+    productos.forEach(element => {
+        var cant = element['cantidades'][0]*hombres + element['cantidades'][1]*mujeres + element['cantidades'][2]*menores;
+        var texto = element['texto'];
+
+        var item = '<div class="col-md-6 px-3 mb-md-3 border-bottom">';
+        item += '<h5><span class="font-weight-bold">'+ cant + ' Kg</span>&nbsp;'+ texto + '</h5>';
+        item += '</div>';
+
+        items.append(item);
     });
 
 }
